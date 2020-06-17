ed6c852cc932a352f39e8be20cd574e231722a96
ghassanachi <ghassan.achi@gmail.com>
2020-06-17 21:45:01 +0300

    Add meta description

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="descritption" content="Ghassan Gedeon Achi's personal website. Software developer with a keen interest in DevOps" />
+	<meta name="description" content="Ghassan Gedeon Achi's personal website. Software developer with a keen interest in DevOps" />
         <title>Ghassan's Website</title>
       </Head>
       <div id="main">{children}</div>
